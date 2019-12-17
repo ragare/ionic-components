79f231786300258697a0d37ed7a06dcf3e608b72
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2019-12-17 17:15:27 +0100

    ionic-popover (2)

--- src/app/pages/popover/popover.page.ts
+++ src/app/pages/popover/popover.page.ts
@@ -14,11 +14,17 @@ export class PopoverPage implements OnInit {
   ngOnInit() {
   }
 
-  async mostrarPop() {
+  async mostrarPop(evento) {
     const popover = await this.popoverCtrl.create({
       component: PopinfoComponent,
+      event: evento,
+      mode: 'ios',
+      backdropDismiss: false
     });
     await popover.present();
+    // const {data} = await popover.onDidDismiss();
+    const {data} = await popover.onWillDismiss();
+    console.log("Data: ", data);
   }
 
 }
